b7b3d474e6c752212f96dca42b47d6939103cfd6
Parthiv Akilesh <71423865+ParthivAkilesh@users.noreply.github.com>
2022-03-21 19:10:47 +0530

    Create script.js

---
+++ Portfolio Website/script.js
@@ -0,0 +1,2 @@
+link2 = () => alert("Go to: https://share.streamlit.io/parthivakilesh/bank-authenticator-ml-model/main/app.py#bank-authenticator")
+link1 = () => alert("Go to: https://share.streamlit.io/parthivakilesh/diabetes_classification/main/app1.py")
